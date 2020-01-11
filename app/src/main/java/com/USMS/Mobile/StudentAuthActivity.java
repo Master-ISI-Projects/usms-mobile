ba83a2c2b7935dee0443838317c8b0e68ac88d09
El attari Yassine <yassineattari64@gmail.com>
2020-01-11 16:19:41 +0100

    Added student result activity and floating action button

--- app/src/main/java/com/USMS/Mobile/StudentAuthActivity.java
+++ app/src/main/java/com/USMS/Mobile/StudentAuthActivity.java
@@ -37,8 +37,9 @@ public class StudentAuthActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(StudentAuthActivity.this,
+                        com.USMS.Mobile.Student_notification_Activity.class);
+                startActivity(intent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
--- app/src/main/java/com/USMS/Mobile/Student_result_Activity.java
+++ app/src/main/java/com/USMS/Mobile/Student_result_Activity.java
@@ -1,11 +1,14 @@
 package com.USMS.Mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.USMS.Mobile.models.NotificationItem;
+import com.USMS.Mobile.models.ResultItem;
+import com.USMS.Mobile.models.ResultItemAdapter;
 
 import java.util.ArrayList;
 
@@ -13,7 +16,7 @@ public class Student_result_Activity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter myAdapter;
     RecyclerView.LayoutManager layoutManager;
-    ArrayList<NotificationItem> itemList;
+    ArrayList<ResultItem> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,22 @@ public class Student_result_Activity extends AppCompatActivity {
         setTitle(R.string.menu_student_result);
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = findViewById(R.id.result_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        itemList = new ArrayList<ResultItem>();
+        itemList.add(new ResultItem("Module 1", "att 1", "attTwo 1"));
+        itemList.add(new ResultItem("Module 2", "att 2", "attTwo 2"));
+        itemList.add(new ResultItem("Module 3", "att 3", "attTwo 3"));
+        itemList.add(new ResultItem("Module 4", "att 4", "attTwo 4"));
+
+
+        myAdapter = new ResultItemAdapter(this, itemList);
+        recyclerView.setAdapter(myAdapter);
+
     }
     @Override
     public boolean onSupportNavigateUp(){
