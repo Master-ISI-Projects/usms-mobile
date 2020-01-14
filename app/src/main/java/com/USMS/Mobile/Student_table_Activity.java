501d7b5e3b885f758428fff809923759150981a1
El attari Yassine <yassineattari64@gmail.com>
2020-01-14 17:41:09 +0100

    [UI & API] Added display table & more

--- app/src/main/java/com/USMS/Mobile/Student_table_Activity.java
+++ app/src/main/java/com/USMS/Mobile/Student_table_Activity.java
@@ -2,7 +2,11 @@ package com.USMS.Mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableRow;
 
 public class Student_table_Activity extends AppCompatActivity {
 
@@ -14,6 +18,38 @@ public class Student_table_Activity extends AppCompatActivity {
         setTitle(R.string.menu_student_table);
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TableRow trFirst = (TableRow) findViewById(R.id.onClickEmploiTemps);
+        TableRow trSecond = (TableRow) findViewById(R.id.onClickEmploiControle);
+        TableRow trThird = (TableRow) findViewById(R.id.onClickEmploiExams);
+
+        trFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://via.placeholder.com/150";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        trSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://via.placeholder.com/250";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        trThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://via.placeholder.com/350";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 
     @Override
